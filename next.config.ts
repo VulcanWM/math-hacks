3548a8e6e82eedfe036e98bdeefbee314df109fd
VulcanWM <logmedha09@gmail.com>
2025-10-01 10:52:46 +0100

    fixed turbopack package bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    turbopack: {
+        root: __dirname, // ensures the current folder is treated as the root
+    },
 };
 
 export default nextConfig;
